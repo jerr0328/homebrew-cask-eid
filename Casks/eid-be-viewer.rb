925548cbed887198c55499a5ba5098ddcb9f88d4
commitay <commitay@users.noreply.github.com>
2018-01-31 07:26:40 +1000

    Update eid-be-viewer to 4.3.4 (#42)

--- Casks/eid-be-viewer.rb
+++ Casks/eid-be-viewer.rb
@@ -1,8 +1,8 @@
 cask 'eid-be-viewer' do
-  version '4.2.7'
-  sha256 'e9d7ff7028d36fa1a162fec56a8dc2711f03f20b11c5bd5e991b6f79a38bce3d'
+  version '4.3.4'
+  sha256 '61e2e50ffc95aa8560bbb725968dcac4ffceb952ec03e81c33099302f84fe0ec'
 
-  url "https://eid.belgium.be/sites/default/files/software/eid_viewer-#{version}-signed.dmg"
+  url "https://eid.belgium.be/sites/default/files/software/eid_viewer-#{version}.dmg"
   name 'Electronic identity card software of Belgium (viewer)'
   name 'eID Viewer'
   homepage 'http://eid.belgium.be/'
