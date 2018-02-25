63bce7049648307bad7dbe3e6ac9d6a930fb7a6f
Wilfried Daniels <wilfried.daniels@gmail.com>
2018-02-26 00:37:29 +0100

    Update eid-be to use signed version (#47)

--- Casks/eid-be.rb
+++ Casks/eid-be.rb
@@ -1,13 +1,13 @@
 cask 'eid-be' do
   version '4.3.5'
-  sha256 '36969084d8ddc583c8881e30df1ae0d2e04ae289b0ba1d5006c8085a2605008f'
+  sha256 'c6fc5a1eecdaee74411aa3edc498e0e0eb7d105d661272a277e985f8dc67d0b1'
 
-  url "https://eid.belgium.be/sites/default/files/software/eid-quickinstaller-#{version}.dmg"
+  url "https://eid.belgium.be/sites/default/files/software/eid-quickinstaller-#{version}-signed.dmg"
   name 'Electronic identity card software of Belgium'
   name 'eID Belgium Quickinstaller'
   homepage 'http://eid.belgium.be/'
 
-  pkg 'eID-Quickinstaller.pkg'
+  pkg 'eID-Quickinstaller-signed.pkg'
 
   uninstall pkgutil: 'be.eid.middleware'
 
