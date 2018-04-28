c88474702607f2d8e71375463639a13b964da833
Jeremy <jerr0328@users.noreply.github.com>
2018-04-28 14:42:40 +0200

    Update eid-be to 4.3.8 (#51)

--- Casks/eid-be.rb
+++ Casks/eid-be.rb
@@ -1,15 +1,15 @@
 cask 'eid-be' do
-  version '4.3.5'
-  sha256 'c6fc5a1eecdaee74411aa3edc498e0e0eb7d105d661272a277e985f8dc67d0b1'
+  version '4.3.8'
+  sha256 'c3472ccb016eecfe93b7cb3cd4a05af7172d7b1259f21cd8db8ef0b650e38942'
 
-  url "https://eid.belgium.be/sites/default/files/software/eid-quickinstaller-#{version}-signed.dmg"
+  url "https://eid.belgium.be/sites/default/files/software/eid-quickinstaller-#{version}.dmg"
   name 'Electronic identity card software of Belgium'
   name 'eID Belgium Quickinstaller'
-  homepage 'http://eid.belgium.be/'
+  homepage 'https://eid.belgium.be/'
 
   pkg 'eID-Quickinstaller-signed.pkg'
 
-  uninstall pkgutil: 'be.eid.middleware'
+  uninstall pkgutil: 'be.eid.*'
 
   caveats do
     depends_on_java
