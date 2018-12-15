8861e902b162a5f697656a24cad72f7a4e1947f6
Vítor Galvão <info@vitorgalvao.com>
2018-12-15 00:49:18 +0000

    Update eid-pt to 3.0.13.5530 (#61)
    * Update eid-pt to 3.0.13.5530
    
    * Update eid-pt.rb
    
    * Update eid-pt.rb
    
    * Update eid-pt.rb
    
    * Update eid-pt.rb

--- Casks/eid-pt.rb
+++ Casks/eid-pt.rb
@@ -1,19 +1,21 @@
 cask 'eid-pt' do
-  version '2.4.6'
-  sha256 '0f07c26ba9561eca278d8ce20ea6be778be3323602f5c6d4671024910c3aa70c'
+  version '3.0.13.5530'
+  sha256 '01d7c9b8f0e279b2e3e64122374034302ee1a2919f7d01da73958f02fbdde90f'
 
-  url 'https://www.autenticacao.gov.pt/documents/10179/11955/pteid-mw.pkg'
+  url "https://www.autenticacao.gov.pt/documents/10179/11955/Assinatura+Digital+%28MACOS+#{version}%29/9717429b-41c2-404b-ad5b-7c0358ca3bef"
   name 'Cartão de Cidadão'
   name 'Electronic identity card software for Portugal'
   name 'eID Portugal'
   homepage 'https://www.autenticacao.gov.pt/'
 
-  pkg 'pteid-mw.pkg'
+  container type: :pkg
 
-  uninstall pkgutil: 'pt.cartaodecidadao*',
-            signal:  [
-                       ['TERM', 'com.yourcompany.pteidgui'],
-                       ['TERM', 'com.yourcompany.pteiddialogsQTsrv'],
-                       ['TERM', 'pt.cartaodecidadao.dss'],
-                     ]
+  pkg "Assinatura Digital (MACOS #{version}).pkg"
+
+  uninstall pkgutil: 'pt.cartaodecidadao.*',
+            script:  {
+                       executable: '/usr/local/bin/pteid_uninstall.sh',
+                       sudo:       true,
+                     },
+            quit:    'pt.gov.autenticacao'
 end
